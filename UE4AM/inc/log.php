bf317e6487878e1fb831be60ca1b1fb1884c7a98
xzessmedia <tim@xzessmedia.de>
2015-03-17 10:22:32 +0100

    Logs enhanced

--- UE4AM/inc/log.php
+++ UE4AM/inc/log.php
@@ -1,44 +1,57 @@
 <?php
-/*! \brief very simple but helpful log class, which outputs a log.txt on script launch
+/*! \brief very simple but helpful log class, which outputs a u4am.log on script launch
 */
 
 class UE4AM_Log {
 	private $logcontent;
+	private $entries;	// How much entries the class has been made in this session
 	
+
 	function __construct() {
-	file_put_contents("log.txt", 'xLog 0.1 init\n');
+	$entries = 0;
+	file_put_contents("ue4am.log", '*****************************************************'."\r\n".'*****************************************************'."\r\n".'UE4AM new logging init'."\r\n"."Recording Time: ".date("d.m.Y H:i:s")."\r\n",FILE_APPEND);
+	$this->AddPlaceholderLine();
 	    }
     
+    	private function AddPlaceholderLine()
+    	{
+    	 $lineplaceholder = '____________________________________'."\r\n";
+	
+	$olddata = $this->logcontent;
+	$this->logcontent = $olddata."\r\n".$lineplaceholder;
+    	}
+    	
 	public function WriteLog()
 	{
-	
-	file_put_contents('log.txt', $this->logcontent,FILE_APPEND);
+	global $debug;
+	if($debug == true)
+	{
+	echo($this->logcontent);
+	}
+	file_put_contents('ue4am.log', $this->logcontent,FILE_APPEND);
 	}
 	public function AddMessage($var)
 	{
-	$str2add = "[".time()."] >> MESSAGE ".$var;
-	$ldata = $this->logcontent;
-	$this->logcontent = $ldata.'\n'.$str2add;
+	$this->entries += 1;
+	$str2add = "[".$this->entries."] >> MESSAGE ".$var;
+
+	$this->logcontent = $str2add."\r\n";
 	$this->WriteLog();
 	}
 	public function AddLog($var)
 	{
-	global $debug;
-	
-	if($debug == true)
-	{
-	echo($var);
-	}
-	$str2add = "[".time()."] >> ".$var;
-	$ldata = $this->logcontent;
-	$this->logcontent = $ldata.'\n'.$str2add;
+	$this->entries += 1;
+	$str2add = "[".$this->entries."] >> ".$var;
+
+	$this->logcontent = $str2add."\r\n";
 	$this->WriteLog();
 	}
 	public function AddLogArray($array)
 	{
+	$this->entries += 1;
 	$text = print_r($array,true);
-	$ldata = $this->logcontent;
-	$this->logcontent = $ldata.'\n'.$text;
+	
+	$this->logcontent = $text."\r\n";
 	$this->WriteLog();
 	}
 }
